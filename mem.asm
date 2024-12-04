a1da4aed388feb2bb39b0a4ba745e6c11a63891b
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2024-12-03 23:18:52 -0300

    adicionando comentarios nos exemplos para aula

--- mem.asm
+++ mem.asm
@@ -1,12 +1,10 @@
-
 .data
-resposta: 0
+resposta: 0             # Reserva um espaço de memória para a variável "resposta" inicializada com 0
 
 .text
-li $t0, 10 #load 10 em t0
-sw $t0, resposta #store t0 em resposta na memória
+li $t0, 10              # Carrega o valor 10 no registrador $t0
+sw $t0, resposta        # Armazena o valor de $t0 na variável "resposta" na memória
+lw $a0, resposta        # Carrega o conteúdo da variável "resposta" no registrador $a0
+li $v0, 1               # Carrega o código de syscall para imprimir um número inteiro em $v0
+syscall                 # Chama a syscall para imprimir o valor de $a0 (que é o conteúdo de "resposta", ou seja, 10)
 
-lw $a0, resposta #load conteúdo de resposta no registrador a0
-
-li $v0, 1 #load valor da syscall print integer em v0
-syscall #print 
--- recSum.asm
+++ recSum.asm
@@ -2,42 +2,48 @@
 .ent __start
 
 .data
-a: .word 1
-b: .word 3
+a: .word 1              # Reserva o valor 1 em "a"
+b: .word 3              # Reserva o valor 3 em "b"
 
 .text
-__start:lw $a0, a 		#get a
-	lw $a1, b		#get b
-	jal sum 		#sum(a..b)
-	li $v0, 1 		#set syscall to print
-	move $a0, $v1 
-	syscall 		#print
-	
-	li $v0, 10 		#exit
-	syscall
+__start:
+lw $a0, a               # Carrega o valor de "a" no registrador $a0
+lw $a1, b               # Carrega o valor de "b" no registrador $a1
+jal sum                 # Chama a função sum(a, b)
 
-sum:	addi $sp, $sp, -8 	#move stack pointer by 2 words
-	sw $s0, 4($sp) 		#stack push non-temp register
-	sw $ra, 0($sp) 		#stack push return address
-	
-	bgt $a0, $a1, fail 	#if a > b
-	
-	beq $a0, $a1, return 	#elif a == b
-	
-	move $s0, $a1		#move b to aux variable
-	addi $a1, $a1, -1	#b = b - 1
-	jal sum			#sum(a, b - 1)
-	add $v1, $s0, $v1	#return b + sum(a, b - 1)
-	j cleanup
-	
-return: move $v1, $a0 		#return a
-	j cleanup
-	
-fail:	li $v1, 1		#return 1
-	j cleanup
+li $v0, 1               # Define o código de syscall para imprimir um número inteiro em $v0
+move $a0, $v1           # Move o resultado de sum(a, b) para $a0
+syscall                 # Chama a syscall para imprimir o valor de $a0
 
-cleanup:lw $ra, 0($sp) 		#stack pop return address
-	lw $s0, 4($sp) 		#stack pop non-temp register
-	addi $sp, $sp, 8 	#move stack pointer back by 2 words
-	jr $ra 			#return to caller
-	
+li $v0, 10              # Define o código de syscall para finalizar o programa
+syscall                 # Chama a syscall para terminar o programa
+
+sum:
+addi $sp, $sp, -8       # Move o ponteiro da pilha para baixo por 2 palavras (reservando espaço na pilha)
+sw $s0, 4($sp)          # Armazena o registrador $s0 na pilha
+sw $ra, 0($sp)          # Armazena o endereço de retorno ($ra) na pilha
+
+bgt $a0, $a1, fail      # Se a > b, pula para a label "fail"
+
+beq $a0, $a1, return    # Se a == b, pula para a label "return"
+
+move $s0, $a1           # Move o valor de b para $s0 (variável auxiliar)
+addi $a1, $a1, -1       # Decrementa b (b = b - 1)
+jal sum                 # Chama sum(a, b - 1)
+add $v1, $s0, $v1       # Soma b com o resultado de sum(a, b - 1) e armazena no registrador $v1
+j cleanup               # Pula para a label "cleanup" para limpar a pilha e retornar
+
+return:
+move $v1, $a0           # Retorna o valor de a no registrador $v1
+j cleanup               # Pula para a label "cleanup" para limpar a pilha e retornar
+
+fail:
+li $v1, 1               # Retorna 1 no registrador $v1 (caso de erro: a > b)
+j cleanup               # Pula para a label "cleanup" para limpar a pilha e retornar
+
+cleanup:
+lw $ra, 0($sp)          # Restaura o endereço de retorno da pilha
+lw $s0, 4($sp)          # Restaura o valor do registrador $s0 da pilha
+addi $sp, $sp, 8        # Move o ponteiro da pilha de volta (libera o espaço usado na pilha)
+jr $ra                  # Retorna para o chamador (endereço armazenado em $ra)
+
